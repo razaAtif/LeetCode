3baa37e6b6251cfb1c2b75719d711718afc6e170
razaAtif <94505868+razaAtif@users.noreply.github.com>
2022-07-28 18:32:29 +0530

    Time: 248 ms (74.01%), Space: 42.9 MB (57.61%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.java
+++ 686-repeated-string-match/686-repeated-string-match.java
@@ -1,21 +1,18 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-      if(a.contains(b)) return  1;
-        if(a.length() == 1 && b.charAt(0) != b.charAt(b.length()-1)) return -1;
-        else if (a.length() == 1 && b.charAt(0) == b.charAt(b.length()-1))return b.length();
-        int count = 1;
-        String add = a;
-        if(a.length() > b.length()){
-            a = a + add;
-            if(a.contains(b)) return 2;
-            else return -1;
-        }
-        while(!a.contains(b) && a.length() < b.length()*2){
-            a = a + add;
-            count++;
-            if(a.contains(b)) return count;
-        }
-        if(a.contains(b)) return count;
-        return -1;
+      StringBuilder sb = new StringBuilder();
+      int count = 0;
+      while(sb.length() < b.length()){
+          sb.append(a);
+          count++;
+      }
+      if(sb.toString().contains(b)){
+          return count;
+      }
+      sb.append(a);
+      count++;
+      if(sb.toString().contains(b))
+          return count++;
+      return -1;
     }
 }
