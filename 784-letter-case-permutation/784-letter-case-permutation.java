feae7a07b8188ab9f5645c24dc2f41b439d46d6a
razaAtif <94505868+razaAtif@users.noreply.github.com>
2022-09-25 19:22:55 +0530

    Time: 4 ms (78.06%), Space: 42.8 MB (96.29%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.java
+++ 784-letter-case-permutation/784-letter-case-permutation.java
@@ -1,7 +1,5 @@
 class Solution {
     List<String> list = new ArrayList<>();
-    Set<String> set = new LinkedHashSet<>();
-    
     public List<String> letterCasePermutation(String s) {
        StringBuilder op = new StringBuilder();
 
@@ -12,33 +10,26 @@ class Solution {
 
     private void solve (String s, StringBuilder op){
         if(s.length() == 0){
-            if(!set.contains(op.toString())){
-                set.add(op.toString());
-                list.add(op.toString());
-            }
+            list.add(op.toString());
             return;
         }
 
-        StringBuilder op1 = new StringBuilder(op);
-        StringBuilder op2 = new StringBuilder(op);
-
-
         if(Character.isDigit(s.charAt(0))){
+            StringBuilder op1 = new StringBuilder(op);
             op1.append(s.charAt(0));
-            op2.append(s.charAt(0));
+            s = s.substring(1);
+            solve(s, op1);
         }
-        else if(Character.isLowerCase(s.charAt(0))){
-            op1.append(s.charAt(0));
-            op2.append(Character.toUpperCase(s.charAt(0)));
-        }
-        else if(Character.isUpperCase(s.charAt(0))){
+        else {
+            StringBuilder op1 = new StringBuilder(op);
+            StringBuilder op2 = new StringBuilder(op);
+
             op1.append(Character.toLowerCase(s.charAt(0)));
-            op2.append(s.charAt(0));
+            op2.append(Character.toUpperCase(s.charAt(0)));
+
+            s = s.substring(1);
+            solve(s,op1);
+            solve(s,op2);
         }
-
-        s = s.substring(1);
-
-        solve(s,op1);
-        solve(s,op2);
     }
 }
