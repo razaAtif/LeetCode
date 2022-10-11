a7e5a46eced266e086ffccd74bc15c3e69fbd5ce
razaAtif <94505868+razaAtif@users.noreply.github.com>
2022-10-12 00:30:07 +0530

    Time: 3 ms (21.48%), Space: 41.4 MB (27.73%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -1,15 +1,16 @@
 class Solution {
     public int kthFactor(int n, int k) {
          PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i*i <= n; i++) {
             if(n % i == 0){
                 maxHeap.add(i);
-                if(maxHeap.size() == k){
-                   return maxHeap.poll();
+                if(n / i != i)  maxHeap.add(n/i);
+                 while(maxHeap.size() > k){
+                    maxHeap.poll();
                 }
             }
         }
-
+        if(maxHeap.size() == k) return maxHeap.peek();
         return -1;
     }
 }
