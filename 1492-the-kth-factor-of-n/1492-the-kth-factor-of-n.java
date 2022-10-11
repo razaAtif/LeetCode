4eb1fa69a79e39e07a00bf9a83b8e9ff37bba5d3
razaAtif <94505868+razaAtif@users.noreply.github.com>
2022-10-12 00:18:58 +0530

    Time: 4 ms (8.66%), Space: 41 MB (50.37%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -4,12 +4,11 @@ class Solution {
         for (int i = 1; i <= n; i++) {
             if(n % i == 0){
                 maxHeap.add(i);
-                if(maxHeap.size() > k){
-                    maxHeap.poll();
+                if(maxHeap.size() == k){
+                   return maxHeap.poll();
                 }
             }
         }
-        if(maxHeap.size() == k) return maxHeap.peek();
 
         return -1;
     }
